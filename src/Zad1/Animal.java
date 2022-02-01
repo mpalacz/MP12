538cb5bfd8ad0d2ac0709bf0e581917769775c3e
DesktopPC <mpalacz00@gmail.com>
2022-02-01 20:43:29 +0100

    Zad 1 d,e

--- src/Zad1/Animal.java
+++ src/Zad1/Animal.java
@@ -2,6 +2,7 @@ package Zad1;
 
 public class Animal {
 }
+
 class Dog extends Animal implements Run,Speak{
     @Override
     public void run() {
@@ -13,6 +14,7 @@ class Dog extends Animal implements Run,Speak{
         System.out.println("Dog barks");
     }
 }
+
 class Cat extends Animal implements Run,Speak{
     @Override
     public void run() {
@@ -24,24 +26,53 @@ class Cat extends Animal implements Run,Speak{
         System.out.println("Cat meows");
     }
 }
-class Shark extends Animal{
 
-}
+class Shark extends Animal{ }
+
 interface Run{
     void run();
 }
+
 interface Speak{
     void speak();
 }
-class HomeAnimal<A extends Animal> {
-    A animal;
 
-    public HomeAnimal(A animal) {
-        this.animal = animal;
+class HomeAnimal<A extends Animal> {
+    A object;
+
+    HomeAnimal(A animal) {
+        this.object = animal;
     }
 
+    A getObject() {
+        return object;
+    }
 
-    public void sleepOnTheCouch(){
+    void showType(){
+        System.out.println("Type A is "+ object.getClass().getName());
+    }
+
+    public void sleepOnTheCouch() {
         System.out.println("Home animal sleeps on the couch.");
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        HomeAnimal<Animal> catHomeAnimal = new HomeAnimal<>(new Cat());
+
+        System.out.println(catHomeAnimal.getObject());
+        catHomeAnimal.showType();
+        catHomeAnimal.sleepOnTheCouch();
+        System.out.println();
+
+        HomeAnimal<Animal> dogHomeAnimal = new HomeAnimal<>(new Dog());
+        System.out.println(dogHomeAnimal.getObject());
+        dogHomeAnimal.showType();
+        dogHomeAnimal.sleepOnTheCouch();
+        System.out.println();
+
+        HomeAnimal<Animal> sharkHomeAnimal = new HomeAnimal<>(new Shark());
+
     }
 }
